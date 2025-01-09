e232bb8b2f06a44f68b00c5129f9908ce5d8661a
johnnyaug <johnnyaug@gmail.com>
2025-01-09 16:20:08 +0200

    clean

--- src/main/kotlin/puzzle14/Main.kt
+++ src/main/kotlin/puzzle14/Main.kt
@@ -6,16 +6,17 @@ private const val w = 101
 data class Point(var y: Int, var x: Int) {
     constructor(l: List<Int>) : this(l[1], l[0])
 
-    fun add(o: Point) {
+    fun add(o: Point): Point {
         y = (y + o.y).mod(h)
         x = (x + o.x).mod(w)
+        return this
     }
 }
 
 data class Robot(val p: Point, var v: Point) {
     constructor(l: List<Point>) : this(l[0], l[1])
 
-    fun click() = p.add(v)
+    fun click(): Point = p.add(v)
 }
 
 class Solution {
@@ -26,18 +27,17 @@ class Solution {
         }.toList()
 
     fun part1(): Long {
-        repeat(100) { robots.forEach(Robot::click) }
-        return robots.filter { it.p.y != h / 2 && it.p.x != w / 2 }.groupingBy {
-            (if (it.p.y < h / 2) "U" else "D") + (if (it.p.x < w / 2) "L" else "R")
+        return robots.map { r ->
+            repeat(100) { r.click() }
+            r.p
+        }.filter { it.y != h / 2 && it.x != w / 2 }.groupingBy {
+            (if (it.y < h / 2) "U" else "D") + (if (it.x < w / 2) "L" else "R")
         }.eachCount().values.fold(1L) { a, b -> a * b.toLong() }
     }
 
     fun part2(): Int {
         (1..h * w).forEach { time ->
-            val board = robots.map { r ->
-                r.click()
-                r.p
-            }.toSet()
+            val board = robots.map { r -> r.click() }.toSet()
             for (y in 0..h) {
                 if ((w / 2 - 7..w / 2 + 7).all { x -> board.contains(Point(y, x)) }) {
                     return time
